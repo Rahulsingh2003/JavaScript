57245231f7bac3bd6e26592e3fcaced8cd76cfca
Rahul <rs9462784650@gmail.com>
2024-09-22 23:54:15 +0530

    all about objects

--- 02_basics/object.js
+++ 02_basics/object.js
@@ -3,11 +3,9 @@
 // // //object literals
 // // // object.create()
 
-
 // // console.log(JsUser.email);
 // // console.log(JsUser["email"]); //sometime good practice
 // // console.log(JsUser[mySym])
-
 
 // // JsUser.email = "LOAM@GMAIL.COM"
 // // // console.log(JsUser.email);
@@ -17,23 +15,20 @@
 
 // // JsUser.greeting = function(){
 //     //     console.log("hello js user ");
-    
+
 //     // }
 //     const mySym = Symbol("key1")
-    
-    
-    
+
 //     const JsUser = {
 //         name: "Rahul",
 //         [mySym]: "key10",
 //         age: 21,
-//         location: "kota", 
+//         location: "kota",
 //         email: "Rahul@gmail.com",
 //         isLoggedIn: false,
 //         lastLoginDays: ["monday","sunday"]
-    
-//     }
 
+//     }
 
 // JsUser.greeting = function() {
 //     console.log(`hello js user ${this.name}`);
@@ -41,31 +36,52 @@
 
 // JsUser.greeting();
 
-
 // const tinderUser = new Object() //singleton object
-const tinderUser = {} // non-singleton object
+const tinderUser = {}; // non-singleton object
 
-tinderUser.id ="123abc"
-tinderUser.name ="Jiji"
-tinderUser.isLoggedIn = false
+tinderUser.id = "123abc";
+tinderUser.name = "Jiji";
+tinderUser.isLoggedIn = false;
 
 // console.log(tinderUser);
 
-const regularUser ={
-    email:"som@gmail.com",
-    fullname: {
-        userfullname:{
-            firstname:"Rahul",
-            lastname:"Singh"
-        }
-    }
-}
+const regularUser = {
+  email: "som@gmail.com",
+  fullname: {
+    userfullname: {
+      firstname: "Rahul",
+      lastname: "Singh",
+    },
+  },
+};
 
-console.log(regularUser.fullname.userfullname.firstname)
+console.log(regularUser.fullname.userfullname.firstname);
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
 
 // const obj3 ={obj1,obj2}
-const obj3 = Object.assign({},obj1,obj2)
-console.log(obj3);
+// const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 = { ...obj1, ...obj2 };
+// console.log(obj3);
+
+const Users = [
+  {
+    id: 1,
+    email: "ccc@gmail.com",
+  },
+  {},
+  {},
+  {},
+];
+
+Users[1].email;
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
