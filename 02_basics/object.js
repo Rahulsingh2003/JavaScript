a30e3940475316b854595917fc08f630f44302b1
Rahul singh <89693207+Rahulsingh2003@users.noreply.github.com>
2024-09-22 17:10:37 +0000

    saving

--- 02_basics/object.js
+++ 02_basics/object.js
@@ -1,47 +1,71 @@
-// //singleton
+// // //singleton
 
-// //object literals
-// // object.create()
-
-
-// console.log(JsUser.email);
-// console.log(JsUser["email"]); //sometime good practice
-// console.log(JsUser[mySym])
+// // //object literals
+// // // object.create()
 
 
-// JsUser.email = "LOAM@GMAIL.COM"
 // // console.log(JsUser.email);
-// // Object.freeze(JsUser)
+// // console.log(JsUser["email"]); //sometime good practice
+// // console.log(JsUser[mySym])
 
-// console.log(JsUser);
 
-// JsUser.greeting = function(){
-    //     console.log("hello js user ");
+// // JsUser.email = "LOAM@GMAIL.COM"
+// // // console.log(JsUser.email);
+// // // Object.freeze(JsUser)
+
+// // console.log(JsUser);
+
+// // JsUser.greeting = function(){
+//     //     console.log("hello js user ");
     
-    // }
-    const mySym = Symbol("key1")
+//     // }
+//     const mySym = Symbol("key1")
     
     
     
-    const JsUser = {
-        name: "Rahul",
-        [mySym]: "key10",
-        age: 21,
-        location: "kota", 
-        email: "Rahul@gmail.com",
-        isLoggedIn: false,
-        lastLoginDays: ["monday","sunday"]
+//     const JsUser = {
+//         name: "Rahul",
+//         [mySym]: "key10",
+//         age: 21,
+//         location: "kota", 
+//         email: "Rahul@gmail.com",
+//         isLoggedIn: false,
+//         lastLoginDays: ["monday","sunday"]
     
+//     }
+
+
+// JsUser.greeting = function() {
+//     console.log(`hello js user ${this.name}`);
+// }
+
+// JsUser.greeting();
+
+
+// const tinderUser = new Object() //singleton object
+const tinderUser = {} // non-singleton object
+
+tinderUser.id ="123abc"
+tinderUser.name ="Jiji"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser ={
+    email:"som@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname:"Rahul",
+            lastname:"Singh"
+        }
     }
-
-
-JsUser.greeting = function() {
-    console.log(`hello js user ${this.name}`);
 }
 
-JsUser.greeting();
+console.log(regularUser.fullname.userfullname.firstname)
 
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
 
-
-
-
+// const obj3 ={obj1,obj2}
+const obj3 = Object.assign({},obj1,obj2)
+console.log(obj3);
