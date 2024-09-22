82c110e03099b3bb55a158cd726940e431949d6a
Rahul singh <89693207+Rahulsingh2003@users.noreply.github.com>
2024-09-22 09:52:22 +0000

    learn about Datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //this means treat all JS code as newer version
+
+
+// alert("Hello") //error cuz we are using nodejs
+
+// console.log(2+2)
+
+// datatypes
+
+let name = "Rahul"  //string
+let age = 21 //int
+let isloggedIn = false //bool
+
+// many more 
+/*
+number
+bigint
+string
+boolean
+null => standalone value
+undefined =>
+symbol => unique   will learn in react
+object
+*/
+
+// console.log((typeof "")); learn about which type of variable it is
+ //null type is object
+ //undefined type is undefiend 
+
+
