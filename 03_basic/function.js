b4138ada652bb2688622235c4dbf229197522baf
Rahul <rs9462784650@gmail.com>
2024-09-23 00:39:41 +0530

    about function

--- 03_basic/function.js
+++ 03_basic/function.js
@@ -25,3 +25,32 @@ function loginUserMessage(username) {
 // console.log(loginUserMessage("Rahul"));
 
 // console.log(loginUserMessage());
+
+function calculateCartPrice(a, b, ...num1) {
+  //rest opreater
+  return num1;
+}
+// console.log(calculateCartPrice(200, 300, 440, 500, 600));
+
+const user = {
+  userName: "Rahul",
+  prices: 2311,
+};
+function handleObject(anyObject) {
+  console.log(
+    `Username is ${anyObject.userName} and Price is ${anyObject.price}`
+  );
+}
+
+handleObject({
+  userName: "ram",
+  price: 455,
+});
+
+const myArr = [1, 2, 3, 4, 5];
+
+function returnSecondValue(getArr) {
+  return getArr[1];
+}
+
+// console.log(returnSecondValue(myArr));
