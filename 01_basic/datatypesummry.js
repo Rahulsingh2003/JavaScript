896179ec3b893b8a18686831d0bd2bb7de8732a4
Rahul singh <89693207+Rahulsingh2003@users.noreply.github.com>
2024-09-22 10:40:00 +0000

    about heap and stack memory

--- 01_basic/datatypesummry.js
+++ 01_basic/datatypesummry.js
@@ -1,37 +1,68 @@
-//  Primitive
+// //  Primitive
 
-//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+// //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
+// // const bigNumber = 3456543576654356754n
 
 
 
-// Reference (Non primitive)
+// // Reference (Non primitive)
 
-// Array, Objects, Functions
+// // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
-// https://262.ecma-international.org/5.1/#sec-11.4.3
+// // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// //+++++++++++++++++++++++++++
+
+// //stack (primitive), heap (non-primitive)
+
+// let myName = "Rahul"
+// let anotherName = myName
+// anotherName = "Singh"
+// console.log(anotherName);
+
+
+// let user = {
+//     email: "xxx.gmail.com",
+//     upi: "user1"
+
+// }
+
+// let user2 = user
+
+// user2.email = "ggg@gmail.com"
+
+// console.log(user.email);
+// console.log(user2);
+
+
+
+
+
+
