3ab2eb3945e758b4ac5d24dd9df3319c1352a77b
Rahul singh <89693207+Rahulsingh2003@users.noreply.github.com>
2024-09-22 09:38:19 +0000

    about variables

---
+++ 01_basic/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = 14453
+let accountEmail = "xxx@gmail.com"
+var accountPassword = "1234" // -> dont use yyou can use it but dont
+accountCity = "Kota" // -> Should not use
+let accountState;
+
+// accountId = 2 not allowed
+
+// allowed 
+accountEmail = 'jjj@gmail.com'
+accountPassword = "3423441"
+accountCity = "Bang"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
